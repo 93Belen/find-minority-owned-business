b6aacae0c831c87541b974a61ef4b1d2dda3b98d
Belen Garcia Martinez <93bgarciamartinez@gmail.com>
2022-01-29 14:19:04 -0600

    got each name in one div

--- public/index.js
+++ public/index.js
@@ -6,22 +6,30 @@ async function getApiFromServer(){
     const response = await fetch('/api');
     const json = await response.json();
     //console.log(json);
-    const arr = []
-    for(let i = 0; i < 10; i++){
-        arr.push([json.businesses[i].name, json.businesses[i].url, json.businesses[i].image_url]);
-        console.log(arr);
-    }
 
     for(let j = 0; j < 10; j++){
         const div = document.createElement('div');
-        document.getElementById('response').appendChild(div).id = 'div' + j;
+        const divId = document.getElementById('response').appendChild(div).id = 'div' + j;
         console.log(div);
 
+        document.getElementById('div' + j).innerHTML = `<h2>${json.businesses[j].name}</h2>`
 
     }
 
-    document.getElementById('response').innerHTML = arr;
+    
 
+
+
+/*
+    const arr = []
+    for(let i = 0; i < 10; i++){
+        arr.push([json.businesses[i].name, json.businesses[i].url, json.businesses[i].image_url]);
+        document.getElementById('div' + i).innerHTML = arr[i];
+    }
+    console.log(arr);
+
+    document.getElementById('response').innerHTML = arr;
+*/
 }
 
 getApiFromServer();
