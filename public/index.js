05d61c866d28e368a0a7296e0776418b46e04afc
Belen Garcia Martinez <93bgarciamartinez@gmail.com>
2022-01-29 14:04:25 -0600

    made 10 divs and an array of 10 arrays

--- public/index.js
+++ public/index.js
@@ -5,12 +5,27 @@ document.getElementById('hola').style.color = 'pink';
 async function getApiFromServer(){
     const response = await fetch('/api');
     const json = await response.json();
-    console.log(json);
+    //console.log(json);
     const arr = []
-    for(let i = 0; i < 5; i++){
-        arr.push(json.businesses[i].name);
+    for(let i = 0; i < 10; i++){
+        arr.push([json.businesses[i].name, json.businesses[i].url, json.businesses[i].image_url]);
+        console.log(arr);
     }
-    document.getElementById('response').textContent = arr;
+
+    for(let j = 0; j < 10; j++){
+        const div = document.createElement('div');
+        document.getElementById('response').appendChild(div).id = 'div' + j;
+        console.log(div);
+
+
+    }
+
+    document.getElementById('response').innerHTML = arr;
+
 }
 
 getApiFromServer();
+
+
+
+
