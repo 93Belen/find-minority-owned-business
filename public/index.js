dc4962687fc7854dfb5c9dc48a544536431041e4
Belen Garcia Martinez <93bgarciamartinez@gmail.com>
2022-01-31 15:50:51 -0600

    got everything without using  innerHtml

--- public/index.js
+++ public/index.js
@@ -1,42 +1,72 @@
 
-
-document.getElementById('hola').style.color = 'pink';
-
 async function getApiFromServer(){
-    const response = await fetch('/api');
+    const feedNum = 20;
+    const city = 'KansasCity';
+    const term = 'coffee';
+    const response = await fetch(`api/${city}/${term}`);
     const json = await response.json();
-    //console.log(json);
+    console.log(json);
 
-    for(let j = 0; j < 10; j++){
+
+    // Creates the divs for the feed
+
+    for(let j = 0; j < feedNum; j++){
         const div = document.createElement('div');
+        const h2 = document.createElement('h2');
+        const h3 = document.createElement('h3');
+        const p1 = document.createElement('p');
+        const p2 = document.createElement('p');
+        const p3 = document.createElement('p');
+        const img = document.createElement('img');
+        const a = document.createElement('a');
 
         document.getElementById('response').appendChild(div).id = 'div' + j;
         console.log(div);
+        document.getElementById('div' + j).appendChild(h2).id = 'h2' + j;
+        document.getElementById('div' + j).appendChild(h3).id = 'categories' + j;
+        document.getElementById('div' + j).appendChild(p1).id = 'address' + j;
+        document.getElementById('address' + j).className = 'address';
+        document.getElementById('div' + j).appendChild(img).id = 'img' +j;
+        document.getElementById('div' + j).appendChild(a).id = 'a' + j;
+        document.getElementById('div' + j).appendChild(p2).id = 'rating' + j;
+        document.getElementById('rating' + j).className = 'rating';
+        document.getElementById('div' + j).appendChild(p3).id = 'review-count' + j;
+        document.getElementById('review-count' + j).className = 'review-count';
+        document.getElementById('div' + j).appendChild(a).id = 'a' + j;
 
-        document.getElementById('div' + j).innerHTML = `<h2>${json.businesses[j].name}</h2><br><a href="${json.businesses[j].url}" target:"_blank">Visit website</a>`;
 
-        document.getElementById('div' + j).style.backgroundImage = `url(${json.businesses[j].image_url})`;
+        // get the titles from json response
+
+        function getTitles() {
+            const p = [];
+            for(let i = 0; i < json.businesses[j].categories.length; i++){
+            p.push(`${json.businesses[j].categories[i].title}`)
+            }
+        return p.join(' ~ ');
+        }
+
+        // creates the inner structure of the feed **need to change the innerHtml**
+        document.getElementById('h2' + j).textContent = json.businesses[j].name;
+        document.getElementById('categories' + j).textContent = getTitles();
+        document.getElementById('address' + j).textContent = 'Address: ' + json.businesses[j].location.address1 + ', ' + json.businesses[j].location.city + ', ZipCode : ' + json.businesses[j].location.zip_code;
+        document.getElementById('img' + j).src = json.businesses[j].image_url;
+        document.getElementById('rating' + j).textContent = `Rating: ${json.businesses[j].rating}`;
+        document.getElementById('review-count' + j).textContent = `Reviews: ${json.businesses[j].review_count}`;
+        document.getElementById('a' + j).href = json.businesses[j].url;
+        document.getElementById('a' + j).textContent = 'Visit website';
 
 
     }
 
-    
 
 
 
-/*
-    const arr = []
-    for(let i = 0; i < 10; i++){
-        arr.push([json.businesses[i].name, json.businesses[i].url, json.businesses[i].image_url]);
-        document.getElementById('div' + i).innerHTML = arr[i];
-    }
-    console.log(arr);
 
-    document.getElementById('response').innerHTML = arr;
-*/
 }
 
 getApiFromServer();
+
+
 
 
 
