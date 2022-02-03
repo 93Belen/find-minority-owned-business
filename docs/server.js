fcf87546f7ae69fc9b0093f9fdc4e363a94f3270
Belen Garcia Martinez <93bgarciamartinez@gmail.com>
2022-02-03 16:04:14 -0600

    server inside docs

---
+++ docs/server.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import fetch from 'node-fetch';
+import 'dotenv/config';
+
+
+const app = express();
+
+app.use(express.static('./'));
+
+
+app.get('/', (req, res) => {
+    res.sendFile('./')
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening at ${port}`));
+
+
+app.get('/api/:city/:term/:owner', async (req, res) => {
+    const city = req.params.city;
+    const term = req.params.term;
+    const owner = req.params.owner;
+    const api_key = process.env.API_KEY;
+    const fetchApi = await fetch(`https://api.yelp.com/v3/businesses/search?location=${city}&term=${owner}+${term}&radius=9000&sort_by=rating`, {
+         method: 'GET',
+         headers: {
+              Authorization : api_key
+         }
+    });
+    const apiResponse = await fetchApi.json();
+    console.log(apiResponse);
+    res.json(apiResponse);
+})
+
